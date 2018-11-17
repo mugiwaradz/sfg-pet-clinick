61353305c4d0342b6a1826e28f90033a3785cb7d
azb <azb@audaxis.com>
2018-11-17 09:23:26 +0100

    test git branch

--- src/main/java/com/spring/sfgpetclinick/SfgPetClinickApplication.java
+++ src/main/java/com/spring/sfgpetclinick/SfgPetClinickApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgPetClinickApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgPetClinickApplication.class, args);
+
+
+        System.out.println("git push");
+        SpringApplication.run(SfgPetClinickApplication.class, args
+
+        );
     }
 }
