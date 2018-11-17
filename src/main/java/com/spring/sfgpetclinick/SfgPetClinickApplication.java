98f8b1a4dcb577186a4ba99003e0f7bc0d95c352
azb <azb@audaxis.com>
2018-11-17 09:12:06 +0100

    add initial project

---
+++ src/main/java/com/spring/sfgpetclinick/SfgPetClinickApplication.java
@@ -0,0 +1,12 @@
+package com.spring.sfgpetclinick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinickApplication.class, args);
+    }
+}
